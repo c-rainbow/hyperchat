46ac0c14fde07db90a890db3a90ab1c1fb242594
c-rainbow <46095062+c-rainbow@users.noreply.github.com>
2022-06-19 23:06:39 -0700

    Translate to English by default

--- main/lib/translator.ts
+++ main/lib/translator.ts
@@ -4,7 +4,7 @@ import tr from 'googletrans';
 
 export async function translate(message: string) {
   const result = await tr(message, {
-    to: 'ko'  // TODO: Get target language from config
+    to: 'en'  // TODO: Get target language from config
   });
 
   return result;
